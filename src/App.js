7de03515e8355c9a3f233fde13f82eac8ea83a83
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2019-02-27 15:36:54 +0200

    Implement dynamic mdx rendering with runtime

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MDX from '@mdx-js/runtime'
+
+// This JSON data is in reality fetched from an external API.
+const apiData = {
+  section1: "# Header\n\nThis is some content\n\n<Alert type=\"warning\">\nRemind to initialize the stream first!\n</Alert>\n"
+}
+
+const Alert = (props) => <div className="alert" style={{ color: 'red' }}>{props.children}</div>
 
 class App extends Component {
   render() {
+    // Provide custom components that will be referenced as JSX
+    // in the markdown string
+    const scope = {
+      Alert
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MDX scope={scope}>{apiData.section1}</MDX>
       </div>
     );
   }
